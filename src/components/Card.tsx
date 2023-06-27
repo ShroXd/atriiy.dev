e350370e6d026b4f57f73d3b07c81c405874e665
Atriiy <ShroXdF@gmail.com>
2023-06-27 22:40:54 +0800

    Optimize css style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,23 +10,23 @@ export interface Props {
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
   const { title, pubDatetime, description } = frontmatter;
   return (
-    <li className="my-6">
+    <li className="mb-8">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="inline-block font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
         {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          <h2 className="text-xl font-medium decoration-dashed hover:underline">
             {title}
           </h2>
         ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
+          <h3 className="text-xl font-medium decoration-dashed hover:underline">
             {title}
           </h3>
         )}
       </a>
-      <Datetime datetime={pubDatetime} />
-      <p>{description}</p>
+      <p className="mt-1 text-sm text-skin-base opacity-50">{description}</p>
+      <Datetime className="mt-3" datetime={pubDatetime} />
     </li>
   );
 }
