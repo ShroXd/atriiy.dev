0bcebc8d6e4df090c19d6970bc030e82101afdae
Atriiy <48713853+ShroXd@users.noreply.github.com>
2025-02-22 16:30:26 +0800

    feat: udpate rss config and 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,13 @@
+import FuzzyText from './components/FuzzyText/FuzzyText'
+
 export default function NotFound() {
   return (
     <section>
-      <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
-        404 - Page Not Found
-      </h1>
+      <div className='mb-12'>
+        <FuzzyText baseIntensity={0.2} hoverIntensity={0.4} enableHover={true}>
+          404
+        </FuzzyText>
+      </div>
       <p className='mb-4'>The page you are looking for does not exist.</p>
     </section>
   )
