a09f9f81d24783e64fb38146c840da6a8dcabdf7
Atriiy <48713853+ShroXd@users.noreply.github.com>
2025-02-22 17:46:03 +0800

    fix: fix content shaking on the mobile screen

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,14 @@ export default function Page() {
       <h1 className='mb-4 text-2xl font-semibold tracking-tighter'>
         Hi, I'm Atriiy.
       </h1>
-      <DecryptedText
-        text={`Dev, using TypeScript, Rust and Golang. Vim fans. Eighteen plus eleven years old now, still have hair, wanna learn many things. Writing, reading, playing video games.`}
-        animateOn='view'
-        speed={30}
-        sequential
-      />
+      <div className='h-28 md:h-20 lg:h-20'>
+        <DecryptedText
+          text={`Dev, using TypeScript, Rust and Golang. Vim fans. Eighteen plus eleven years old now, still have hair, wanna learn many things. Writing, reading, playing video games.`}
+          animateOn='view'
+          speed={30}
+          sequential
+        />
+      </div>
       <div className='my-8'>
         <BlogPosts />
       </div>
