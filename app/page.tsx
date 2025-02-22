5b87854eb13c80f44f3e07b22e3b2a0a627d2da4
Atriiy <48713853+ShroXd@users.noreply.github.com>
2025-02-22 15:59:51 +0800

    feat: update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,19 @@
 import { BlogPosts } from 'app/components/posts'
 
-const introduction = (
-  <>
-    Hi, I'm Atriiy, an interest-driven dev.
-    <br />
-    <br />
-    I'm not good at introducing myself not only in reality but also on the web.
-    <br />
-    But even if I can escape, I still have to face it.
-    <br />
-    So, the following is my introduction.
-    <br />
-    <br />
-    Dev, using TypeScript, Rust and Golang. Vim fans.
-    <br />
-    <br />
-    Eighteen plus eleven years old now, still have hair, wanna learn many
-    things.
-    <br />
-    Writing, reading, playing video games.
-    <br />
-    <br />
-    Ok, stop right here.
-  </>
-)
+import DecryptedText from './components/DecryptedText/DecryptedText'
 
 export default function Page() {
   return (
     <section>
-      <h1 className='mb-8 text-2xl font-semibold tracking-tighter'>
-        My Portfolio
+      <h1 className='mb-4 text-2xl font-semibold tracking-tighter'>
+        Hi, I'm Atriiy.
       </h1>
-      <p className='mb-4'>{introduction}</p>
+      <DecryptedText
+        text={`Dev, using TypeScript, Rust and Golang. Vim fans. Eighteen plus eleven years old now, still have hair, wanna learn many things. Writing, reading, playing video games.`}
+        animateOn='view'
+        speed={30}
+        sequential
+      />
       <div className='my-8'>
         <BlogPosts />
       </div>
