f57e80b8b9cebb27ab81f11abc82d9975ca03e42
Atriiy <48713853+ShroXd@users.noreply.github.com>
2025-02-23 11:21:02 +0800

    fix: fix color in dark mode

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,32 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
 import FuzzyText from './components/FuzzyText/FuzzyText'
 
 export default function NotFound() {
+  const [isDark, setIsDark] = useState(false)
+
+  useEffect(() => {
+    setIsDark(window.matchMedia('(prefers-color-scheme: dark)').matches)
+
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+    const handler = (e: MediaQueryListEvent) => setIsDark(e.matches)
+
+    mediaQuery.addEventListener('change', handler)
+
+    return () => mediaQuery.removeEventListener('change', handler)
+  }, [])
+
   return (
     <section>
       <div className='mb-12'>
-        <FuzzyText baseIntensity={0.2} hoverIntensity={0.4} enableHover={true}>
+        <FuzzyText
+          baseIntensity={0.2}
+          hoverIntensity={0.4}
+          enableHover={true}
+          color={isDark ? 'white' : 'black'}
+        >
           404
         </FuzzyText>
       </div>
