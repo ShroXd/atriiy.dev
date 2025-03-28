bc3deede64524b4f1ef14905e2d97707a2cee12f
Atriiy <shroxdf@gmail.com>
2025-03-28 23:32:03 +0800

    feat: refine AnimatedLink component with improved styling and hover effects

--- app/components/AnimatedLink.tsx
+++ app/components/AnimatedLink.tsx
@@ -26,17 +26,14 @@ export default function AnimatedLink({
   const Component = isHash ? 'a' : Link
 
   return (
-    <motion.span
-      className='inline-block'
-      whileHover={{ y: -1 }}
-      whileTap={{ scale: 0.98 }}
-    >
+    <motion.span className='relative inline-block' whileTap={{ scale: 0.98 }}>
       <Component
         href={href}
-        className={`relative inline-block border-b border-[#6e7554] pb-[0.5px] text-[#6e7554] transition-all hover:border-[#6e7554] [.table-of-contents_&]:border-0 ${className}`}
+        className={`group relative inline-block text-[#6e7554] transition-all [.table-of-contents_&]:border-0 ${className}`}
         {...linkProps}
       >
         {children}
+        <span className='absolute bottom-0 left-1/2 h-[1px] w-0 bg-[#6e7554] transition-all duration-400 ease-[linear(0,_0.008_1.1%,_0.031_2.2%,_0.129_4.8%,_0.257_7.2%,_0.671_14.2%,_0.789_16.5%,_0.881_18.6%,_0.957_20.7%,_1.019_22.9%,_1.063_25.1%,_1.094_27.4%,_1.114_30.7%,_1.112_34.5%,_1.018_49.9%,_0.99_59.1%,_1)] group-hover:left-0 group-hover:w-full' />
       </Component>
     </motion.span>
   )
